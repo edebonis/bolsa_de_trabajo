11df6e57274ece5f0152047feb3c93bcc965b837
Esteban <edebonis@sagradoalcal.edu.ar>
2019-09-14 18:08:22 -0300

    Agregada aplicación bolsa a admin.

--- bolsa/admin.py
+++ bolsa/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Oportunidad, Tipo
 
 # Register your models here.
+admin.site.register(Oportunidad)
+admin.site.register(Tipo)
--- bolsa/models.py
+++ bolsa/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+import django
+
+class Tipo(models.Model):
+    nombre = models.CharField(max_length=50)
+    relacionado = models.ManyToManyField("self", blank=True)
+
+    def __str__(self):
+        return self.nombre
 
 
 class Oportunidad(models.Model):
@@ -7,8 +15,10 @@ class Oportunidad(models.Model):
     descripcion = models.TextField(max_length=5000)
     visible = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return '{} - {} - {}'.format(self.id, self.tipo, self.user)
 
-class Tipo(models.Model):
-    nombre = models.CharField(max_length=50)
-    relacionado = models.ManyToManyField(self)
+class Usuario(User):
+    
